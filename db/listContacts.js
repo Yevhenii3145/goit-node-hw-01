735e7c5b1a54b6c6530e1ac57f4a9344387548f5
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-12-01 16:24:51 +0200

    console

--- db/listContacts.js
+++ db/listContacts.js
@@ -5,7 +5,7 @@ const contactsPath = require("./filePath");
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
   const contacts = JSON.parse(data);
-  console.log(contacts[1]);
+  // console.log(contacts[1]);
   return contacts;
 };
 
